32021ac57871bd64a2912324775965088fe8abe3
Nithichai <rockon23146@gmail.com>
2019-03-24 16:08:37 +0700

    Update

--- index.js
+++ index.js
@@ -34,19 +34,25 @@ app.use(require('express-session')({secret: 'keyboard dog', resave: true, saveUn
 // Initialize Passport and restore authentication state, if any, from the session.
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Define routes.
-app.get('/', cors(),  (req, res, next) => {
+app.get('/', (req, res) => {
   if (req.user == undefined) {
     res.redirect('/login/line')
   } else {
-    res.sendStatus(200)
+    res.status(200)
+    res.end()
   }
 })
 
-app.get('/login/line', passport.authenticate('line'));
+app.get('/login/line', cors(), passport.authenticate('line'));
 
-app.get('/login/line/return', passport.authenticate('line', {failureRedirect: '/'}), function(req, res, next) {
+app.get('/login/line/return', cors(), passport.authenticate('line', {failureRedirect: '/'}), function(req, res, next) {
   axios({
     method: 'post',
     url: 'https://bhcd-api.herokuapp.com/user-info/new',
@@ -63,15 +69,15 @@ app.get('/login/line/return', passport.authenticate('line', {failureRedirect: '/
     }
   }).then((response) => {
     console.log(response.data.data)
-    // res.status(200)
-    next()
+    res.status(200)
+    res.end()
   }).catch((error) => {
-    console.log(error.message)
-    res.redirect('/login/line')
+    res.status(404)
+    res.end()
   })
 });
 
-app.get('/logout', function(req, res){
+app.get('/logout', cors(), function(req, res){
   axios({
     method: 'post',
     url: 'https://bhcd-api.herokuapp.com/user-info/delete/id',
@@ -84,11 +90,11 @@ app.get('/logout', function(req, res){
         }
     }
   }).then((response) => {
-    console.log(response.data.data)
-    res.sendStatus(200)
+    res.status(200)
+    res.end()
   }).catch((error) => {
-    console.log(error.message)
-    res.sendStatus(400)
+    res.status(404)
+    res.end()
   })
 });
 
