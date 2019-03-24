9fd340f1eac76ec8fe4a1924a81529843a2009ee
Nithichai <rockon23146@gmail.com>
2019-03-24 21:19:24 +0700

    Update

--- index.js
+++ index.js
@@ -71,7 +71,16 @@ app.get('/login/line/return', cors(), passport.authenticate('line', {failureRedi
     }
   }).then((response) => {
     // res.redirect("https://basic-health-care-device.herokuapp.com/#")
-    res.redirect("http://127.0.0.1:8080/#")
+    // res.redirect("http://127.0.0.1:8080/#")
+    axios({
+      method: 'get',
+      url: 'https://bhcd-line-login.herokuapp.com',
+    }).then((response) => {
+      next()
+    }).catch((error) => {
+      window.location.replace("https://bhcd-line-login.herokuapp.com/login/line")
+      next()
+    })
   }).catch((error) => {
     res.redirect("/login/line")
   })
